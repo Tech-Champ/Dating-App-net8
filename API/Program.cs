d238293efadde8f7ed085dcce9bd591cb308733d
chanchlesh <chanchlesh.surya1c98@gmail.com>
2024-10-15 08:35:56 +0530

    ng initial commit

--- API/Program.cs
+++ API/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddDbContext<DataContext>(opt =>
 //// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 //builder.Services.AddEndpointsApiExplorer();
 //builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -26,7 +26,8 @@ var app = builder.Build();
 //app.UseHttpsRedirection();
 
 //app.UseAuthorization();
-
+app.UseCors(x=> x.AllowAnyHeader().AllowAnyMethod()
+.WithOrigins("http://localhost:4200","https://localhost:4200/"));
 app.MapControllers();
 
 app.Run();
